1158d8cbb5ec4d734e094ebf7bf4e8efc00a9770
Andrew Chen <andrew@jaanuu.com>
2024-06-23 16:06:24 -0700

    should be move

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 rclone mkdir sftp:mongobackup
 mongodump --uri="$MONGO_URI" --archive=/tmp/backup
-rclone copyto /tmp/backup "sftp:/mongobackup/$BACKUP_DIR/$(date +%Y-%m-%d_%H-%M-%S).dump"
+rclone moveto /tmp/backup "sftp:/mongobackup/$BACKUP_DIR/$(date +%Y-%m-%d_%H-%M-%S).dump"
