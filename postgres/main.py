f43c15b32f3fb2e3a8e9fbb554272d494bc69d30
Andrew Chen <andrew@jaanuu.com>
2025-02-07 09:12:43 -0800

    disablr

--- postgres/main.py
+++ postgres/main.py
@@ -33,14 +33,14 @@ def job_with_argument(name):
     print(f"I am {name}")
 
 def backup_db():
-    rclone_config_path = os.environ.get('RCLONE_CONFIG_PATH', './rclone.conf')
-    subprocess.run(['rclone', '--config', rclone_config_path, 'mkdir', 'sftp:pgbackup'], check=True)
+    remote_prefix = '{}'.format(os.environ['PGBACKUPDIR'])
+    subprocess.run(['rclone', '--config', '/notfound', 'mkdir', f'remote:{remote_prefix}'], check=True)
     with tempfile.TemporaryDirectory() as tmpdirname:
         databases = json.loads(subprocess.check_output(['psql', '-c', "SELECT json_agg(datname) FROM pg_database WHERE datname NOT IN ('template0', 'template1', 'postgres')",
                                             '--tuples-only', '--no-align']))
         now = datetime.now(timezone.utc)
         ts=now.strftime('%H_%M_%S')
-        remote_folder='{}/{}'.format(os.environ['PGBACKUPDIR'],now.strftime('%Y/%m/%d'))
+        remote_folder='{}/{}'.format(remote_prefix,now.strftime('%Y/%m/%d'))
         for database in databases:
             print("backing up",database)
             output = f'{tmpdirname}/{database}.sql'
@@ -48,8 +48,8 @@ def backup_db():
             subprocess.run(['pg_dump', database, '--column-inserts', '-f', output], check=True)
             #Path(output).touch()
 
-            # rclone moveto /tmp/backup "sftp:/mongobackup/$BACKUP_DIR/$(date +%Y-%m-%d_%H-%M-%S).dump"
-            subprocess.run(['rclone', '-v', '--config', rclone_config_path, 'moveto', output, f'sftp:{remote_folder}/{ts}/{database}.sql'], check=True)
+            # rclone moveto /tmp/backup "remote:/mongobackup/$BACKUP_DIR/$(date +%Y-%m-%d_%H-%M-%S).dump"
+            subprocess.run(['rclone', '-v', '--config', '/notfound', 'moveto', output, f'remote:{remote_folder}/{ts}/{database}.sql'], check=True)
 
 if __name__ == '__main__':
     #backup_db()
@@ -57,7 +57,6 @@ if __name__ == '__main__':
     #schedule.every(10).seconds.do(job_with_argument, name="Peter")
     #schedule.every(10).seconds.do(backup_db)
 
-    backup_db()
     schedule.every().day.at("11:30", "Etc/UTC").do(backup_db)
 
     while True:
